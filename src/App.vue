24c076ca445a9b5b3b78ebc0dcd97edfcaadc5ff
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-10-16 20:06:40 -0300

    applying Tarjeta component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
-<script setup>
+<script>
+import Tarjeta from "./components/Tarjeta.vue";
 
+export default {
+  components: {
+    Tarjeta,
+  },
+  data() {
+    return {
+      articulos: [
+        {
+          nombre: "Martillo",
+          descripcion:
+            "Herramienta de percusión compuesta de una cabeza, por lo común de hierro, y un mango, generalmente de madera.",
+        },
+        {
+          nombre: "Destornillador",
+          descripcion:
+            "Un destornillador es una herramienta que se utiliza para apretar y aflojar tornillos y otros elementos de máquinas que requieren poca fuerza de apriete.",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <div>
-
+    <div v-for="articulo in articulos" :key="articulo">
+      <Tarjeta :nombre="articulo.nombre" :descripcion="articulo.descripcion" />
+    </div>
   </div>
-
 </template>
 
 <style scoped>
