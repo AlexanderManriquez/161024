9a6aee6187aacaf67c8cfe9c52d782af55e1f920
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-10-16 20:25:22 -0300

    adding Producto.vue component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 <script>
 import Tarjeta from "./components/Tarjeta.vue";
 import Avatar from "./components/Avatar.vue";
+import Producto from "./components/Producto.vue";
 
 export default {
   components: {
-    Tarjeta, Avatar
+    Tarjeta, Avatar, Producto
   },
   data() {
     return {
@@ -36,6 +37,11 @@ export default {
     <Avatar
       imgProfile="https://scontent.fscl17-1.fna.fbcdn.net/v/t39.30808-6/227408029_205778354892214_6013986529669216619_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHXm87Zt91OcN90wKmlaNSyr968WOg0f7mv3rxY6DR_ucLHn392lppO8f9PghXyydKSi4UgLP3LFBDclqeqn_u_&_nc_ohc=35qEzpJP2_cQ7kNvgGIvpHz&_nc_zt=23&_nc_ht=scontent.fscl17-1.fna&_nc_gid=A3r8lW1ah-TNJ6SL0I0421B&oh=00_AYAJoCMDgLGvCQ_ht86FCki2o34ECJXvXS7gw4XajIlhsw&oe=6715FBAC"
       size="300"
+    />
+
+    <Producto
+      nombre="Pack de 6 vasos"
+      precio="$ 5990"
     />
   </div>
 </template>
---
+++ src/components/Producto.vue
@@ -0,0 +1,20 @@
+<!-- 2.- Pasar Props con Números
+Crea un componente Producto que reciba el nombre y precio de un producto como props y muestre la información en un formato legible. -->
+
+<template>
+    <div class="card articulo">
+      <div class="card-body">
+        <h1>{{ nombre }}</h1>
+        <p>{{ precio }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Producto",
+    props: ["nombre", "precio"],
+  };
+  </script>
+  
+  <style></style>
